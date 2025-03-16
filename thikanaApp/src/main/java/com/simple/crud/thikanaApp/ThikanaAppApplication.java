8d9fd4823a76ea41bed65ff56683eb8284231361
Jabiullah <inventoryprottoy181@gmail.com>
2025-03-16 16:39:33 +0600

    initial commit

---
+++ thikanaApp/src/main/java/com/simple/crud/thikanaApp/ThikanaAppApplication.java
@@ -0,0 +1,13 @@
+package com.simple.crud.thikanaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThikanaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThikanaAppApplication.class, args);
+	}
+
+}
