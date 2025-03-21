a4b9f0a8ad4fe44187116178b491810fae3ac746
jabiullah <inventoryprottoy@gmail.com>
2025-03-22 02:56:18 +0600

    UserService added

---
+++ thikanaApp/src/main/java/com/simple/crud/thikanaApp/service/UserService.java
@@ -0,0 +1,19 @@
+package com.simple.crud.thikanaApp.service;
+
+import com.simple.crud.thikanaApp.entity.ThikanaEntry;
+import com.simple.crud.thikanaApp.repository.ThikanaEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+public class UserService {
+    // Here will be only business logics
+    @Autowired
+    UserService 
+
+}
