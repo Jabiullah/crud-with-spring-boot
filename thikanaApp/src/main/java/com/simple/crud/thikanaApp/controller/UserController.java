b9cdc25d72e4f0c3dd02bf4a8cb426cfa52e9b05
jabiullah <inventoryprottoy@gmail.com>
2025-03-22 02:52:43 +0600

    userController added

---
+++ thikanaApp/src/main/java/com/simple/crud/thikanaApp/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.simple.crud.thikanaApp.controller;
+
+
+import com.simple.crud.thikanaApp.entity.ThikanaEntry;
+import com.simple.crud.thikanaApp.service.ThikanaEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+
+}
